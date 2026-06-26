00e5d5a61edbd2fb5161c726f8b09c788779fca2
myushen <mengyuan.shen@outlook.com>
2026-06-26 12:00:57 +1000

    update references

--- R/unharmonised.R
+++ R/unharmonised.R
@@ -44,11 +44,13 @@ UNHARMONISED_URL <- "https://object-store.rc.nectar.org.au/v1/AUTH_06d6e008e3e64
 #'   by = c("file_id_cellNexus_single_cell", "cell_id")
 #' )
 #' }
-#' @references Mangiola, S., M. Milton, N. Ranathunga, C. S. N. Li-Wai-Suen,
-#'   A. Odainic, E. Yang, W. Hutchison et al. "A multi-organ map of the human
-#'   immune system across age, sex and ethnicity." bioRxiv (2023): 2023-06.
-#'   doi:10.1101/2023.06.08.542671.
-#' @source [Mangiola et al.,2023](https://www.biorxiv.org/content/10.1101/2023.06.08.542671v3)
+#' @references Shen, M., Y. Gao, N. Liu, D. Bhuva, M. Milton, J. Henao,
+#'   J. Andrews, E. Yang, C. Zhan, N. Liu, S. Si, J. W. Hutchison,
+#'   M. H. Shakeel, M. Morgan, A. T. Papenfuss, J. Iskander, J. M. Polo,
+#'   and S. Mangiola. "cellNexus: Quality control, annotation, aggregation
+#'   and analytical layers for the Human Cell Atlas data." bioRxiv (2026).
+#'   doi:10.64898/2026.04.14.718336.
+#' @source [Shen et al.,2026](https://www.biorxiv.org/content/10.64898/2026.04.14.718336v3)
 get_unharmonised_dataset <- function(
   dataset_id,
   cells = NULL,
